cdbc59d3b538695ca46fa608acb51a3a22488207
KiptooMannu <Kkgg7241@gmail.com>
2024-05-18 18:47:01 +0300

    first commit

---
+++ src/Card.jsx
@@ -0,0 +1,42 @@
+
+import React from 'react';
+
+import profilepic from './assets/car.jpg'
+function Card() {
+
+return(
+
+<div className="card">
+    <img  src={profilepic} alt="profilepic" className="profilepic" />
+   
+  
+        <h2>Kiptoo Emmanuel</h2>
+        <p>Lorem cididunt ut aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+   
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+);
+
+
+}
+export default Card
